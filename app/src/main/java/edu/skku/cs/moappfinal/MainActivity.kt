d032de00623ad06566c2f18d97f02843da8d112e
JunHyeong Park <162807062+Durumi-TheRedCrownedCrane@users.noreply.github.com>
2025-03-20 22:37:37 +0900

    Update MainActivity.kt

--- app/src/main/java/edu/skku/cs/moappfinal/MainActivity.kt
+++ app/src/main/java/edu/skku/cs/moappfinal/MainActivity.kt
@@ -18,7 +18,7 @@ import java.time.format.DateTimeFormatter
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity(), CoroutineScope {
-    private val API_KEY = "35f87caec8404a1885df223a1ff86692"
+    private val API_KEY = ""
     private val GENRE = "CCCA"
     private lateinit var binding: ActivityMainBinding
     private val job = Job()
